fe904a086ba00653dde934a304dba9f9e4b5df15
Silas Brumwell <silas13579@gmail.com>
2015-02-03 19:01:19 -0500

    Merged camera branch.
    Merged camera branch into working test, so that we can test our camera.

---
+++ RobotProject/src/org/usfirst/frc2337/RobotProject/commands/CAMERA_StartUp.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc2337.RobotProject.commands;
+
+import org.usfirst.frc2337.RobotProject.Robot;
+import org.usfirst.frc2337.RobotProject.RobotMap;
+import org.usfirst.frc2337.RobotProject.subsystems.Camera;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.command.Command;
+/**
+ *
+ */
+public class CAMERA_StartUp extends Command {
+
+    public CAMERA_StartUp() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.Camera);
+
+    }
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	RobotMap.usbCamera1.startAutomaticCapture("cam0");
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    	
+
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
