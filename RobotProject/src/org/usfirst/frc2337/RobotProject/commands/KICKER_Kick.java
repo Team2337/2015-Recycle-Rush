27bc62543a52da97f448205ec2a49d59a9cebd52
Silas Brumwell <silas13579@gmail.com>
2015-02-05 21:10:57 -0500

    Added functions to stop autokick, and some button fixes.
    Two new functions, one for a manual kick, and one to stop autokick, were
    added. These both, of course, need to be tested. Also, certain buttons
    were incorrectly setup, so those were fixed.

---
+++ RobotProject/src/org/usfirst/frc2337/RobotProject/commands/KICKER_Kick.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc2337.RobotProject.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc2337.RobotProject.RobotMap;
+import org.usfirst.frc2337.RobotProject.Robot;
+
+/**
+ *
+ */
+public class KICKER_Kick extends Command {
+
+    public KICKER_Kick() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.kicker);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.kicker.kickOut();
+    	Robot.kicker.kickIn();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
