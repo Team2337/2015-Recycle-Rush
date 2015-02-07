cb4ebdd7d0853f664f9ac2423f6216e37fda0d04
Programman7 <brendan1@ameritech.net>
2015-02-07 15:40:21 -0500

    Remove Limited Switches
    Removed the switches for stopping the motor.

--- RobotProject/src/org/usfirst/frc2337/RobotProject/commands/LIFT_JoystickControl.java
+++ RobotProject/src/org/usfirst/frc2337/RobotProject/commands/LIFT_JoystickControl.java
@@ -88,7 +88,8 @@ public class  LIFT_JoystickControl extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (Robot.lift.AtLowerLimit() || Robot.lift.AtUpperLimit());
+        //return (Robot.lift.AtLowerLimit() || Robot.lift.AtUpperLimit());
+    	return false;
     }
 
     // Called once after isFinished returns true
