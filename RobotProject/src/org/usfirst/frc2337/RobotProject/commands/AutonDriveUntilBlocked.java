36255b25a47060912045da768cfefb0ee00c567b
rhyser9 <rhys.barrie@gmail.com>
2015-02-12 15:00:58 -0500

    Un-reverted my inadvertent revert, Robin

--- RobotProject/src/org/usfirst/frc2337/RobotProject/commands/AutonDriveUntilBlocked.java
+++ RobotProject/src/org/usfirst/frc2337/RobotProject/commands/AutonDriveUntilBlocked.java
@@ -31,16 +31,17 @@ public class  AutonDriveUntilBlocked extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	setTimeout(3);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.chassis.driveMecanum(1, 0, 0);
+    	Robot.chassis.driveMecanum(-0.5, 0, 0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.chassis.isToteSensor();
+        return (Robot.chassis.isToteSensor() || isTimedOut());
     }
 
     // Called once after isFinished returns true
@@ -51,6 +52,6 @@ public class  AutonDriveUntilBlocked extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.chassis.stopMotors();
+    	end();
     }
 }
